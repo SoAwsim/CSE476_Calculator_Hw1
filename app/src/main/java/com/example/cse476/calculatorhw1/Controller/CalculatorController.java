7ddf635dc7e26cd459beefae681832b2e504da4a
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-02 22:42:47 +0300

    refactor: make fields private

--- app/src/main/java/com/example/cse476/calculatorhw1/Controller/CalculatorController.java
+++ app/src/main/java/com/example/cse476/calculatorhw1/Controller/CalculatorController.java
@@ -5,8 +5,8 @@ import android.widget.TextView;
 import com.example.cse476.calculatorhw1.Calculator.Calculator;
 
 public class CalculatorController {
-    private Calculator _calculator;
-    private TextView _formulaView;
+    private final Calculator _calculator;
+    private final TextView _formulaView;
 
     public CalculatorController(Calculator calculator, TextView formulaView) {
         this._calculator = calculator;
