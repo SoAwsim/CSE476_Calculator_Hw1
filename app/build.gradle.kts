9a03d253f6931f9ceb345d494824025234c46447
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-02 20:03:46 +0300

    feat: initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.apache.tools.ant.util.JavaEnvUtils.VERSION_11
+
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.cse476.calculatorhw1"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.cse476.calculatorhw1"
+        minSdk = 21
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
