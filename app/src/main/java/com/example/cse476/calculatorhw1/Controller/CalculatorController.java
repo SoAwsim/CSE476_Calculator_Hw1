8edcf814a432ce84a343b5bcb2bb5a56bc8d5706
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-03 14:59:55 +0300

    feat: add state controls for dot and math operations

--- app/src/main/java/com/example/cse476/calculatorhw1/Controller/CalculatorController.java
+++ app/src/main/java/com/example/cse476/calculatorhw1/Controller/CalculatorController.java
@@ -5,26 +5,38 @@ import android.widget.TextView;
 
 import com.example.cse476.calculatorhw1.Calculator.Calculator;
 
+@SuppressLint("SetTextI18n")
 public class CalculatorController {
+    private static final char[] operators = {'+', '-', 'x', '/'};
     private final Calculator _calculator;
     private final TextView _formulaView;
+    private boolean _dotAllowed;
+    private OperationState _operationState;
 
     public CalculatorController(Calculator calculator, TextView formulaView) {
         this._calculator = calculator;
         this._formulaView = formulaView;
+        this._dotAllowed = false;
+        this._operationState = OperationState.MINUS_ALLOWED;
     }
 
-    @SuppressLint("SetTextI18n")
     public void EnterDigit(int digit) {
-        this._formulaView.setText(this._formulaView.getText().toString() + digit);
+        var newFormula = this._formulaView.getText().toString() + digit;
+        this.DetermineDotCanBePlaced(newFormula);
+        this._operationState = OperationState.ALL_OPERATORS_ALLOWED;
+        this._formulaView.setText(newFormula);
     }
 
     public void EnterOperation(char operation) {
-
+        this._dotAllowed = false;
     }
 
     public void EnterDot() {
-
+        if (!this._dotAllowed)
+            return;
+        this._dotAllowed = false;
+        this._operationState = OperationState.ALL_OPERATORS_BANNED;
+        this._formulaView.setText(this._formulaView.getText().toString() + '.');
     }
 
     public void Delete() {
@@ -33,10 +45,46 @@ public class CalculatorController {
             return;
 
         text = text.subSequence(0, text.length() - 1);
+        this.DetermineDotCanBePlaced(text.toString());
+        this.DetermineOperationState(text.toString());
+
         this._formulaView.setText(text);
     }
 
     public void Calculate() {
 
+    }
+
+    private void DetermineDotCanBePlaced(String formula) {
+        if (formula.isEmpty()) {
+            this._dotAllowed = false;
+            return;
+        }
+
+        var indexOfLastOperator = Integer.MIN_VALUE;
+        for (var c : operators) {
+            var foundIndex = formula.lastIndexOf(c);
+            if (foundIndex > indexOfLastOperator)
+                indexOfLastOperator = foundIndex;
+        }
+
+        var indexOfLastDot = formula.lastIndexOf('.');
+        this._dotAllowed = indexOfLastDot <= indexOfLastOperator;
+    }
+
+    private void DetermineOperationState(String formula) {
+        if (formula.isEmpty()) {
+            this._operationState = OperationState.MINUS_ALLOWED;
+            return;
+        }
+
+        var lastChar = formula.charAt(formula.length() - 1);
+
+        if (Character.isDigit(lastChar))
+            this._operationState = OperationState.ALL_OPERATORS_ALLOWED;
+        else if (lastChar == '/' || lastChar == 'x')
+            this._operationState = OperationState.MINUS_ALLOWED;
+        else
+            this._operationState = OperationState.ALL_OPERATORS_BANNED;
     }
 }
---
+++ app/src/main/java/com/example/cse476/calculatorhw1/Controller/OperationState.java
@@ -0,0 +1,7 @@
+package com.example.cse476.calculatorhw1.Controller;
+
+public enum OperationState {
+    ALL_OPERATORS_ALLOWED,
+    ALL_OPERATORS_BANNED,
+    MINUS_ALLOWED
+}
