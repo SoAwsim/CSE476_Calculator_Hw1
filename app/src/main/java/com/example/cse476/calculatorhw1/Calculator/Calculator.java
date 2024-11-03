e2ddaf9f6048ce7d545524779ed19dcfa3e181de
Oğuzhan İçelliler <oguzhan.icelliler@gmail.com>
2024-11-03 19:47:51 +0300

    feat: add support for addition and subtraction

--- app/src/main/java/com/example/cse476/calculatorhw1/Calculator/Calculator.java
+++ app/src/main/java/com/example/cse476/calculatorhw1/Calculator/Calculator.java
@@ -6,6 +6,9 @@ public class Calculator implements ICalculator {
     @Override
     public void SolveFormula(StringBuilder formula) throws ArithmeticException, NumberFormatException {
         this._currentFormula = formula;
+        if (this._currentFormula.length() == 0)
+            return;
+
         this.SolveMultiplicationAndDivision();
         this.SolveAdditionAndSubtraction();
     }
@@ -15,7 +18,7 @@ public class Calculator implements ICalculator {
         var indexOfDivisionOperation = this._currentFormula.indexOf("/");
 
         while (indexOfMultiplicationOperation >= 0 || indexOfDivisionOperation >= 0) {
-            var operationIndexToProcess = DetermineNextOperationIsMultiplyOrDivision(
+            var operationIndexToProcess = DetermineNextOperation(
                     indexOfMultiplicationOperation, indexOfDivisionOperation);
             var operationIsMultiply = operationIndexToProcess == indexOfMultiplicationOperation;
 
@@ -42,7 +45,42 @@ public class Calculator implements ICalculator {
     }
 
     private void SolveAdditionAndSubtraction() {
+        var indexOfAdditionOperation = this._currentFormula.indexOf("+");
 
+        int indexOfSubtractionOperation;
+        if (this._currentFormula.charAt(0) == '-')
+            indexOfSubtractionOperation = this._currentFormula.substring(1).indexOf("-");
+        else
+            indexOfSubtractionOperation = this._currentFormula.indexOf("-");
+
+        while (indexOfAdditionOperation >= 0 || indexOfSubtractionOperation >= 0) {
+            var operationIndexToProcess = DetermineNextOperation(
+                    indexOfAdditionOperation, indexOfSubtractionOperation);
+            var operationIsAddition = operationIndexToProcess == indexOfAdditionOperation;
+
+            var constraints = new MathOperationConstraints(operationIndexToProcess);
+
+            var leftNumber = Double.parseDouble(
+                    this._currentFormula.substring(
+                            constraints.LeftNumberStartIndex, operationIndexToProcess));
+            var rightNumber = Double.parseDouble(
+                    this._currentFormula.substring(
+                            operationIndexToProcess + 1, constraints.RightNumberEndIndex));
+
+            double result;
+            if (operationIsAddition)
+                result = leftNumber + rightNumber;
+            else
+                result = leftNumber - rightNumber;
+
+            this.ValidateAndWriteResult(result, constraints);
+
+            indexOfAdditionOperation = this._currentFormula.indexOf("+");
+            if (this._currentFormula.charAt(0) == '-')
+                indexOfSubtractionOperation = this._currentFormula.substring(1).indexOf("-");
+            else
+                indexOfSubtractionOperation = this._currentFormula.indexOf("-");
+        }
     }
 
     private void ValidateAndWriteResult(double result, MathOperationConstraints constraints) {
@@ -62,18 +100,18 @@ public class Calculator implements ICalculator {
                 resultAsObject.toString());
     }
 
-    private static int DetermineNextOperationIsMultiplyOrDivision(
-            int indexOfMultiplicationOperation,
-            int indexOfDivisionOperation) {
-        if (indexOfMultiplicationOperation >= 0 && indexOfDivisionOperation < 0) {
-            return indexOfMultiplicationOperation;
+    private static int DetermineNextOperation(
+            int indexOfOperation0,
+            int indexOfOperation1) {
+        if (indexOfOperation0 >= 0 && indexOfOperation1 < 0) {
+            return indexOfOperation0;
         }
 
-        if (indexOfMultiplicationOperation < 0) {
-            return indexOfDivisionOperation;
+        if (indexOfOperation0 < 0) {
+            return indexOfOperation1;
         }
 
-        return Math.min(indexOfMultiplicationOperation, indexOfDivisionOperation);
+        return Math.min(indexOfOperation0, indexOfOperation1);
     }
 
     private class MathOperationConstraints {
@@ -83,7 +121,10 @@ public class Calculator implements ICalculator {
         public MathOperationConstraints(int operationIndex) {
             this.LeftNumberStartIndex = ICalculator.FindIndexOfLastOperation(
                     _currentFormula.substring(0, operationIndex)) + 1;
-            if (this.LeftNumberStartIndex != 0 && _currentFormula.charAt(this.LeftNumberStartIndex - 1) == '-')
+            if (
+                    this.LeftNumberStartIndex != 0 &&
+                    _currentFormula.charAt(this.LeftNumberStartIndex - 1) == '-'
+            )
                 this.LeftNumberStartIndex--;
 
             this.RightNumberEndIndex = ICalculator.FindIndexOfFirstOperation(
